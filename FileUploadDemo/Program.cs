c4abf8f62d7642077e4b3320b06ca502db9a81c3
Vasil Rizov <vasilrizov14@gmail.com>
2019-02-01 13:26:09 +0200

    First commit

---
+++ FileUploadDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace FileUploadDemo
+{
+    class Program
+    {
+        private static object qbig;
+        private static object sbtc;
+
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\Users\vasilpc\Desktop\Jars\");
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+            driver.Url = "https://www.google.bg/imghp?hl=bg&tab=wi";
+            driver.FindElement(By.XPath("//*[@id='sbtc']/div/div[2]/div[1]/span")).Click();
+
+            driver.FindElement(By.XPath("//*[@id='qbug']/div/a")).Click();
+            IWebElement fileUpload = driver.FindElement(By.XPath("//*[@id='qbfile']"));
+
+            fileUpload.SendKeys(@"C:\Users\vasilpc\Desktop\3ey7r0sqm7p01.jpg");
+        }
+    }
+}
